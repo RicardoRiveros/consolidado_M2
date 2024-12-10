4d7c93fd876746eaa28486e2fe0e9afcb7b6ffaa
unknown <ricardoriverosturra@gmail.com>
2024-12-10 11:45:34 -0300

    version_2

--- Assets/JS/logica.js
+++ Assets/JS/logica.js
@@ -1,10 +1,10 @@
 document.getElementById('form').addEventListener('submit', function (event) {
-    event.preventDefault(); // Evita el envío real del formulario
+    event.preventDefault();
 
     // Oculta el formulario
     document.getElementById('form').style.display = 'none';
 
-    // Muestra el mensaje de confirmación
+    // Muestra el mensaje de confirmación del envio de formulario
     document.getElementById('confirmation-message').style.display = 'block';
 });
 
